8043aa29a4e898894c7e07ff34032ba234a1c0a9
dkeum <dkeum@sfu.ca>
2024-02-08 19:28:45 -0800

    fixing

--- sanity/schema/lib/client.ts
+++ sanity/schema/lib/client.ts
@@ -14,6 +14,6 @@ export const writeClient = createClient({
   dataset,
   projectId,
   useCdn,
-  token: "skKi4g6QyAAVA0u6js8E9t2zz4OWcRc9UMZIR0tyDaNLBrF8yl5NTBnk5n2JecAGfwyEo3NVHFWX5bAG5i4aLoLCbDaCFKQ5EFnq5fGQJHO94X3mB1Cf9fiAA8PhTOBWd2TyNtBk950249vs4XTvpuzIdjEqgehuUDqOHigLol1yj44U9dda",
+  token,
 })
 
